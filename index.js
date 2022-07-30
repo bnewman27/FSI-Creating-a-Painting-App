2e95c1ae0d07e081e770788ea677f559da7e3dd4
Ben Newman <ben.newman52@ymail.com>
2022-07-30 12:13:48 -0700

    added colors

---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+const painting = document.querySelector('.painting')
+
+painting.addEventListener('click', function(e){
+	e.target.style.backgroundColor = selectedColor 
+	let selectedColor = 'blue'
+const painting = document.querySelector('.painting')
+}
+painting.addEventListener('click', function(e){
+	e.target.style.backgroundColor = selectedColor 
+	let selectedColor = 'yellow'
+const painting = document.querySelector('.painting')
+}
+painting.addEventListener('click', function(e){
+	e.target.style.backgroundColor = selectedColor 
+	let selectedColor = 'red'
+const painting = document.querySelector('.painting')
+}
+painting.addEventListener('click', function(e){
+	e.target.style.backgroundColor = selectedColor 
+	let selectedColor = 'green'
+	const painting = document.querySelector('.painting')
+})
+
+
+
+
+
