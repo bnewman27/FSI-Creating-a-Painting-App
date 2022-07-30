df4ddefa2348904f21611e993a5fa9541961885a
Ben Newman <ben.newman52@ymail.com>
2022-07-30 12:30:02 -0700

    added color

--- index.js
+++ index.js
@@ -5,22 +5,22 @@ const painting = document.querySelector('.painting')
 painting.addEventListener('click', function(e){
 	e.target.style.backgroundColor = selectedColor 
 	let selectedColor = 'blue'
-const painting = document.querySelector('.painting')
 }
 painting.addEventListener('click', function(e){
 	e.target.style.backgroundColor = selectedColor 
 	let selectedColor = 'yellow'
-const painting = document.querySelector('.painting')
-}
+})
 painting.addEventListener('click', function(e){
 	e.target.style.backgroundColor = selectedColor 
 	let selectedColor = 'red'
-const painting = document.querySelector('.painting')
-}
+})
 painting.addEventListener('click', function(e){
 	e.target.style.backgroundColor = selectedColor 
 	let selectedColor = 'green'
-	const painting = document.querySelector('.painting')
+})
+painting.addEventListener('click', function(e){
+	e.target.style.backgroundColor = selectedColor 
+	let selectedColor = 'purple'
 })
 
 
